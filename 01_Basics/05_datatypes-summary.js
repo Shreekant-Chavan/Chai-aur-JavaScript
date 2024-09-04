8cdb917ff72cb227d5bfabf03a56b75aa04cd317
Shreekant Chavan <shreekantchavanfycs@gmail.com>
2024-09-04 15:01:50 +0530

    Data Types Summary

---
+++ 01_Basics/05_datatypes-summary.js
@@ -0,0 +1,60 @@
+// Data Types - 
+// 1. Primitives 
+// 2. Reference ( Non-Primitives )
+
+
+// 1. Primitives : 
+
+// 7 Types => String, Number, Boolean, BigInt, Undefined, Null, Symbol
+
+
+let userName = "Shreekant"  // String
+const score = 100  // number
+const scoreValue = 100.69  // number
+
+const isLoggedIn = false // Boolean
+const outsideTemp = null  // Null
+let userEmail;  // Undefined
+let userEmailId = undefined  // Undefined
+
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log( id === anotherId );  // false
+
+const bigNumber = 6969696969696969696969n
+
+
+
+// 2. Reference ( Non-Primitives ) :
+
+// 3 Types => Array, Object, Functions
+// [], {}, ()
+
+let heros = ["Shaktiman", "Naagraj", "Doga"]
+let myObj = {
+    name : "Shreekant",
+    age : 18,
+}
+
+
+const myFunction = function() {
+    console.log("Hello World!");
+}
+
+
+
+
+
+console.log(typeof bigNumber);  // BigInt
+console.log(typeof outsideTemp);  // Object
+console.log(typeof scoreValue);  // Number
+console.log(typeof userName);  // String
+console.log(typeof userEmail);  // Undefined
+console.log(typeof isLoggedIn);  // Boolean
+console.log(typeof id);  // Symbol
+console.log(typeof anotherId);  // Symbol
+console.log(typeof heros);  // Object
+console.log(typeof myObj);  // Object
+console.log(typeof myFunction);  // Function
