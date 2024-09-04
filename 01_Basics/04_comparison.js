4521c96299d954e3dd8317ad3f5d2fe5d5882e1b
Shreekant Chavan <shreekantchavanfycs@gmail.com>
2024-09-04 14:16:48 +0530

    Comparisons

---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,30 @@
+// console.log( 2 > 1 );
+// console.log( 2 >= 1 );
+// console.log( 2 < 1 );
+// console.log( 2 <= 1 );
+// console.log( 2 == 1 );
+// console.log( 2 != 1 );
+
+
+console.log( "2" > 1 );  // true
+console.log( "02" > 1 );  // true
+
+
+console.log( null > 0 ); // false
+console.log( null >= 0 );  // true
+console.log( null < 0 );  // false
+console.log( null <= 0 );  // true
+console.log( null == 0 );  // false
+
+
+console.log( undefined > 0 ); // false
+console.log( undefined >= 0 );  // false
+console.log( undefined < 0 );  // false
+console.log( undefined <= 0 );  // false
+console.log( undefined == 0 );  // false
+
+
+//  === ( Strict Check => Compares "Values" as well as "Data Type" )
+
+console.log( "2" == 2 );  // true
+console.log( "2" === 2 );  // false
