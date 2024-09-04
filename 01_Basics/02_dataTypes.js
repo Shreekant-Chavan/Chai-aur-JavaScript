341d3b5ba094b7dff3f6d073838e2b5b8449a14a
Shreekant Chavan <shreekantchavanfycs@gmail.com>
2024-09-04 12:36:20 +0530

    Data Types

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+//"use strict" // treat all js code as newer version
+
+// alert ( 3 + 3 ) // We are using Node js not browser
+
+// console.log( 3 
+//     +
+//     3
+// ) // Code readability should be high.
+
+console.log("Hello")
+
+
+let name = "Shreekant"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+// string => ""
+// boolean => true / false
+// null => standalone value
+// undefined => 
+// bigint
+// symbol => unique
+
+
+// object
+
+console.log(typeof "Shree"); // string
+console.log(typeof age);  // number
+console.log(typeof undefined);  // undefined
+console.log(typeof null);  // object
