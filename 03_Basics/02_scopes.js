31c0da9f732e3dfe4b2479c76752222275ad53fe
Shreekant Chavan <shreekantchavanfycs@gmail.com>
2024-09-05 22:23:04 +0530

    Scopes level & Hoisting

--- 03_Basics/02_scopes.js
+++ 03_Basics/02_scopes.js
@@ -11,6 +11,53 @@ if (true) {
     console.log("INNER :" , a);  // a = 10 
 }
 
-console.log(a);  //  Error = a is not defined  ( functional scope )
-console.log(b);  //  Error = b is not defined  ( functional scope )
-console.log(c);  // 30  ( Global Scope)
+// console.log(a);  //  Error = a is not defined  ( functional scope )
+// console.log(b);  //  Error = b is not defined  ( functional scope )
+// console.log(c);  // 30  ( Global Scope)
+
+
+
+function one() {
+    const username = "Shreekant"
+
+    function two() {
+        const website = "Youtube"
+        console.log(username);
+    }
+
+    // console.log(website);  //  Block Scope => It won't run cause "website" is defined under the function two() and it's not accesible over the function
+
+    two()
+}
+
+one()
+
+
+if (true) {
+    const username = "Shreekant"
+    if (username === "Shreekant") {
+        const website = " Youtube"
+        console.log( username + website );
+    }
+    // console.log(website);  //  Block Scope
+}
+
+// console.log(username);  //  Block Scope
+
+
+// +++++++++++++++++++++++++ Intresting +++++++++++++++++++++++++
+
+
+addOne(5)  // This function value accesible before function declaration
+
+function addOne(num) {
+    return num + 1
+}
+
+
+
+const addTwo = function (num) {  //  This function value can't accesible before function declaration,  also called as "Hoisting"
+    return num + 1
+}
+
+addTwo(5)
