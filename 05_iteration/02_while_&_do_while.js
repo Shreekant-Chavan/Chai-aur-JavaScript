f9a7a8529ec6eca84a2bde93d4357ddacf77238b
Shreekant Chavan <shreekantchavanfycs@gmail.com>
2024-09-09 19:45:36 +0530

    While & Do-While

---
+++ 05_iteration/02_while_&_do_while.js
@@ -0,0 +1,42 @@
+//  While
+
+
+let index = 0
+while (index <= 10) {
+    console.log(`Value of index is ${index}`);
+    index = index + 2;
+}
+
+// Value of index is 0
+// Value of index is 2 
+// Value of index is 4 
+// Value of index is 6 
+// Value of index is 8 
+// Value of index is 10
+
+
+let myArray = [ "flash", "batman", "superman"]
+
+let arr = 0
+while (arr < myArray.length) {
+    console.log(`Value is ${myArray[arr]}`);
+    arr = arr + 1
+}
+// Value is flash
+// Value is batman
+// Value is superman
+
+
+
+
+//  Do While Loop
+
+let score = 11
+
+do {
+    console.log(`Score is ${score}`);
+    score++ 
+} while (score <= 10 );
+
+
+// Score is 11
